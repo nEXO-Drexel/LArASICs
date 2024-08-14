c95eeeea2ca50fbf034688b4439e3ef2efc24cb2
brady <brady.r.eckert@gmail.com>
2024-08-14 11:06:42 -0400

    gui now updating the register dict

--- Gui2.py
+++ Gui2.py
@@ -1,10 +1,10 @@
 """
 File Name: Gui2.py
-Author: Kamayani Richhariya (and Brady Eckert)
+Author: Brady Eckert and Kamayani Richhariya
 Email: be348@drexel.edu
 Description: 
 
-Last modified: 2024 Aug 13
+Last modified: 2024 Aug 14
 """
 
 import tkinter as tk
@@ -25,15 +25,15 @@ from cls_config import CLS_CONFIG
 from raw_convertor import RAW_CONV
 
 m = tk.Tk()
-m.geometry("1100x300")
+m.geometry("800x300")
 Title = tk.Label(m, text='LArASIC settings control')
 Title.grid(row=0, column=3)
 
 # radio button variables
 Gain_radio = tk.DoubleVar()  
+Shaping_time_radio = tk.DoubleVar()  
 Channel_baseline_radio = tk.IntVar()  
 SBF_radio = tk.IntVar()  
-Shaping_time_radio = tk.IntVar()  
 Input_radio = tk.IntVar()  
 
 ## functions for using the radio variables to modify the dictionary
@@ -43,19 +43,11 @@ def set_Gain(Gain_radio_dict):
     elif Gain_radio.get() ==  14: return dict(Gain_radio_dict, sg0=0,sg1=1)
     elif Gain_radio.get() ==  25: return dict(Gain_radio_dict, sg0=1,sg1=1)
 
-def set_baseline(Channel_baseline_dict):
-    if   Channel_baseline_radio.get() == 1: return dict(Channel_baseline_dict,snc = 1)
-    elif Channel_baseline_radio.get() == 0: return dict(Channel_baseline_dict,snc = 0)
-
-def set_SBF(SBF_dict):
-    if   SBF_radio.get() == 1: return dict(SBF_dict,sdf= 0)
-    elif SBF_radio.get() == 2: return dict(SBF_dict,sdf= 1)
-
 def set_Shaping_time(Shaping_time_dict):
-    if   Shaping_time_radio.get() == 1: return dict(Shaping_time_dict,st0=1,st1=0) 
-    elif Shaping_time_radio.get() == 2: return dict(Shaping_time_dict,st0=0,st1=0)
-    elif Shaping_time_radio.get() == 3: return dict(Shaping_time_dict,st0=1,st1=1)
-    elif Shaping_time_radio.get() == 4: return dict(Shaping_time_dict,st0=0,st1=1)
+    if   Shaping_time_radio.get() == 0.5: return dict(Shaping_time_dict,st0=1,st1=0) 
+    elif Shaping_time_radio.get() == 1.0: return dict(Shaping_time_dict,st0=0,st1=0)
+    elif Shaping_time_radio.get() == 2.0: return dict(Shaping_time_dict,st0=1,st1=1)
+    elif Shaping_time_radio.get() == 3.0: return dict(Shaping_time_dict,st0=0,st1=1)
 
 def set_Input(Input_dict):
     if Input_radio.get() == 1:
@@ -88,82 +80,69 @@ def set_Input(Input_dict):
 ### Buttons
 def selection_Gain_radio():
     print('Gain set to ',Gain_radio.get(),'mV/fC')
-    #if Gain_radio.get() == 1: print("Gain set to 4.7 mV/fC")
-    #elif Gain_radio.get()==2: print("Gain set to 7.8 mV/fC")
-    #elif Gain_radio.get()==3: print("Gain set to 14 mV/fC")
-    #elif Gain_radio.get()==4: print("Gain set to 25 mV/fC")
 
 Gain = tk.Label(m, text='Gain (mV/fC)').grid(row=1, column=1)
-R1 = tk.Radiobutton(m, text="4.7", variable=Gain_radio, value=4.7, command=selection_Gain_radio).grid(row=2, column=1)  
-R2 = tk.Radiobutton(m, text="7.8", variable=Gain_radio, value=7.8, command=selection_Gain_radio).grid(row=3, column=1)  
-R3 = tk.Radiobutton(m, text="14" , variable=Gain_radio, value=14 , command=selection_Gain_radio).grid(row=4, column=1)  
+R1 = tk.Radiobutton(m, text="4.7", variable=Gain_radio, value=4.7, command=selection_Gain_radio).grid(row=2, column=1)
+R2 = tk.Radiobutton(m, text="7.8", variable=Gain_radio, value=7.8, command=selection_Gain_radio).grid(row=3, column=1)
+R3 = tk.Radiobutton(m, text="14" , variable=Gain_radio, value=14 , command=selection_Gain_radio).grid(row=4, column=1)
 R4 = tk.Radiobutton(m, text="25" , variable=Gain_radio, value=25 , command=selection_Gain_radio).grid(row=5, column=1)
 
 def selection_Channel_baseline():  
-    if Channel_baseline_radio.get()  ==1: print('Channel baseline set to 200mV - collection mode')
-    elif Channel_baseline_radio.get()==2: print('Channel baseline set to 900mV - induction mode')
+    if   Channel_baseline_radio.get()==1: print('Channel baseline set to 200mV - collection mode')
+    elif Channel_baseline_radio.get()==0: print('Channel baseline set to 900mV - induction mode')
 
 Channel_baseline = tk.Label(m, text='Channel baseline').grid(row=1, column=2)
-R5 = tk.Radiobutton(m, text="200 mV - collection mode", variable=Channel_baseline_radio, value=1, command=selection_Channel_baseline).grid(row=2, column=2)  
-R6 = tk.Radiobutton(m, text="900 mV - induction mode" , variable=Channel_baseline_radio, value=0, command=selection_Channel_baseline).grid(row=3, column=2)  
+R5 = tk.Radiobutton(m, text="200 mV - collection mode", variable=Channel_baseline_radio, value=1, command=selection_Channel_baseline).grid(row=2, column=2)
+R6 = tk.Radiobutton(m, text="900 mV - induction mode" , variable=Channel_baseline_radio, value=0, command=selection_Channel_baseline).grid(row=3, column=2)
 
 def selection_SBF():
-    if SBF_radio.get()==1: print('')
-    elif SBF_radio.get()==2: print('')
+    if   SBF_radio.get()==0: print('Bypassing unity buffer')
+    elif SBF_radio.get()==1: print('Unity buffer engaged')
 
 SBF = tk.Label(m, text='SBF').grid(row=1, column=3)
-R7 = tk.Radiobutton(m, text="0 - bypass buffer", variable=SBF_radio, value=1, command=selection_SBF).grid(row=2, column=3)  
-R8 = tk.Radiobutton(m, text="1 - unity buffer", variable=SBF_radio, value=2, command=selection_SBF).grid(row=3, column=3) 
+R7 = tk.Radiobutton(m, text="0 - bypass buffer", variable=SBF_radio, value=0, command=selection_SBF).grid(row=2, column=3)  
+R8 = tk.Radiobutton(m, text="1 - unity buffer" , variable=SBF_radio, value=1, command=selection_SBF).grid(row=3, column=3) 
 
 def selection_Shaping_time():
-    if   Shaping_time_radio.get() == 1: print('Shaping time set to 0.5 us')
-    elif Shaping_time_radio.get() == 2: print('Shaping time set to 1.0 us')
-    elif Shaping_time_radio.get() == 3: print('Shaping time set to 2.0 us')
-    elif Shaping_time_radio.get() == 4: print('Shaping time set to 3.0 us')
+    print('Shaping time set to ',Shaping_time_radio.get(),'us')
 
 Shaping_time = tk.Label(m, text='Shaping_time (us)').grid(row=1, column=4)
+R9  = tk.Radiobutton(m, text="0.5", variable=Shaping_time_radio, value=0.5, command=selection_Shaping_time).grid(row=2, column=4)
+R10 = tk.Radiobutton(m, text="1.0", variable=Shaping_time_radio, value=1.0, command=selection_Shaping_time).grid(row=3, column=4)
+R11 = tk.Radiobutton(m, text="2.0", variable=Shaping_time_radio, value=2.0, command=selection_Shaping_time).grid(row=4, column=4)
+R12 = tk.Radiobutton(m, text="3.0", variable=Shaping_time_radio, value=3.0, command=selection_Shaping_time).grid(row=5, column=4)
 
-R9  = tk.Radiobutton(m, text="0.5", variable=Shaping_time_radio, value=1, command=selection_Shaping_time).grid(row=2, column=4)  
-R10 = tk.Radiobutton(m, text="1.0", variable=Shaping_time_radio, value=2, command=selection_Shaping_time).grid(row=3, column=4)  
-R11 = tk.Radiobutton(m, text="2.0", variable=Shaping_time_radio, value=3, command=selection_Shaping_time).grid(row=4, column=4)  
-R12 = tk.Radiobutton(m, text="3.0", variable=Shaping_time_radio, value=4, command=selection_Shaping_time).grid(row=5, column=4)
+def selection_Input():
+    if   Input_radio.get()==1: print('Test pulse from the FPGA DAC selected')
+    elif Input_radio.get()==2: print('Test pulse from the ASIC DAC selected')
+    elif Input_radio.get()==3: print('FEMB set to receive data from external source')
 
-def selection_Input():  
-   print("You selected the option " + str(Input_radio.get())) 
+Input = tk.Label(m, text='Input').grid(row=1, column=5)
+R13 = tk.Radiobutton(m, text="Test pulse: FPGA DAC", variable=Input_radio, value=1, command=selection_Input).grid(row=2, column=5)
+R14 = tk.Radiobutton(m, text="Test pulse: ASIC DAC", variable=Input_radio, value=2, command=selection_Input).grid(row=3, column=5)
+R15 = tk.Radiobutton(m, text="External source"     , variable=Input_radio, value=3, command=selection_Input).grid(row=4, column=5)
 
-Input = tk.Label(m, text='Input')
-Input.grid(row=1, column=5)
-
-R13 = tk.Radiobutton(m, text="FPGA DAC", variable=Input_radio, value=1, command=selection_Input).grid(row=2, column=5)  
-R14 = tk.Radiobutton(m, text="ASIC DAC", variable=Input_radio, value=2, command=selection_Input).grid(row=3, column=5)  
-R15 = tk.Radiobutton(m, text="External source", variable=Input_radio, value=3, command=selection_Input).grid(row=4, column=5)  
-
-def gobutton():
-    print('you hit go')
+def applybutton():
+    print('Applying settings to register dictionary')
     ### REG SETTINGS, see documentation for explanation of each key
     reg_settings_dict=dict(pls_cs=1, dac_sel=1,\
                     fpgadac_en=1, asicdac_en=0,\
-                    fpgadac_v=0x08,\
+                    fpgadac_v=0x00,\
                     pls_gap = 500,\
                     pls_dly = 10,\
                     mon_cs=0,\
                     data_cs = 0,\
                     sts=1, snc=1, sg0=0, sg1=1, st0=0, st1=0, smn=0, sdf=1,\
                     slk0 = 0, stb1 = 0, stb = 0, s16=0, slk1=0, sdc=0, swdac1=1, swdac2=0, dac=0x0 )
-    print(reg_settings_dict)
     reg_settings_dict=set_Gain(reg_settings_dict)
-    reg_settings_dict=set_baseline(reg_settings_dict)
-    reg_settings_dict=set_SBF(reg_settings_dict)
     reg_settings_dict=set_Shaping_time(reg_settings_dict)
     reg_settings_dict=set_Input(reg_settings_dict)
-    print(reg_settings_dict)
+    reg_settings_dict = dict(reg_settings_dict, sdf=SBF_radio.get(), snc= Channel_baseline_radio.get())
+    #print(reg_settings_dict)
 
-
-
-Go = tk.Button(m, text="GO!", command=gobutton )
-Go.grid(row=6, column=3) 
+Apply = tk.Button(m, text="Apply settings", command=applybutton).grid(row=6, column=3) 
 
 m.mainloop()
 
-
+print('mainloop over')
 #print(reg_settings_dict)
